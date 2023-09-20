11d179bd64303435f4c238441cf64266a2c6c998
DavidZvonaruv5 <102156879+DavidZvonaruv5@users.noreply.github.com>
2023-09-20 16:05:26 +0300

    nodemailer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className='bg-black h-[550px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae nostrum enim fugit voluptatibus et eos repudiandae excepturi, numquam expedita at, possimus aliquid neque unde debitis nam. Id, corrupti laborum.</div>
-  )
+    <div className="bg-[#2b1b18] text-white h-[300px] flex flex-col items-center justify-center">
+      <div className="space-x-8 text-lg">
+        <a href="#home">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="#services">SERVICES</a>
+        <a href="#contact">CONTACT</a>
+      </div>
+      <hr className="w-full border-white my-8"/>
+      <div className="text-xs ">
+        &copy; COPYRIGHT {currentYear} | ALL RIGHTS RESERVED
+      </div>
+    </div>
+  );
 }
