35b5f19e26f1673002d88e31d1e59928ba56a61c
David Zvonaruv <Davidze99@gmail.com>
2023-09-21 12:26:24 +0300

    Finished mail and Gallery

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="bg-[#2b1b18] text-white h-[300px] flex flex-col items-center justify-center">
-      <div className="space-x-8 text-lg">
+    <div className="bg-[#2b1b18] text-white h-[300px] flex flex-col items-center justify-center px-4 sm:px-8 md:px-16 lg:px-32">
+      <div className="space-x-4 sm:space-x-8 text-sm sm:text-lg w-full text-center">
         <a href="#home">HOME</a>
         <a href="#about">ABOUT</a>
         <a href="#services">SERVICES</a>
         <a href="#contact">CONTACT</a>
       </div>
-      <hr className="w-full border-white my-8"/>
-      <div className="text-xs ">
+      <hr className="w-full border-white my-8" />
+      <div className="text-xs sm:text-sm">
         &copy; COPYRIGHT {currentYear} | ALL RIGHTS RESERVED
       </div>
     </div>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Gallery() {
+  const images = [
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg",
+    "/7.jpg",
+  ];
+
   return (
-    <div className='bg-[#ffe4c487]   pt-12 pb-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iste dolores voluptates blanditiis facilis nesciunt numquam dicta! Obcaecati velit, consequuntur voluptatem alias at, animi iusto quae sed commodi tenetur ab magni culpa qui nemo repellat. Vel blanditiis error, quas laborum tempora suscipit velit labore at, maxime ut fuga? Blanditiis, maxime!
+    <div className="bg-[#ffe4c487] pt-12 pb-12">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap justify-center gap-4">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="relative w-[200px] h-[150px] bg-white border-4 border-black rounded-md overflow-hidden"
+            >
+              <div className="absolute top-0 left-0 w-full h-2 bg-black"></div>
+              <div className="absolute bottom-0 left-0 w-full h-2 bg-black"></div>
+              <Image
+                src={image}
+                width={200}
+                height={150}
+                alt={`Gallery Image ${index + 1}`}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
